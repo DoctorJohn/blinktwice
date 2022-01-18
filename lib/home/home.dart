60f813581f4fb0100b25af419b6bd3edebcaec4d
Jonathan Ehwald <github@ehwald.info>
2022-01-18 16:44:29 +0100

    feat: remove trivial examples

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
 
-import '../tasks/start.dart';
-import '../tasks/task1.dart';
-import '../tasks/task2.dart';
-import '../tasks/task2_altn.dart';
+import './widgets/task_button.dart';
 import '../tasks/task3.dart';
 import '../tasks/task4.dart';
-
-import './widgets/task_button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   final Map<String, Widget> _screens = const {
-    "Start": Start(title: "Start"),
-    "Aufgabe 1": Task1(title: "Aufgabe 1"),
-    "Aufgabe 2": Task2(title: "Aufgabe 2"),
-    "Aufgabe 2 Altern.": Task2Altn(title: "Aufgabe 2 Alternative"),
     "Aufgabe 3": Task3(title: "Aufgabe 3"),
     "Aufagbe 4": Task4(title: "eSense Demo"),
   };
 
   List<Widget> _buildButtons() {
-    return _screens.entries.map((e) => TaskButton(name: e.key, page: e.value)).toList();
+    return _screens.entries
+        .map((e) => TaskButton(name: e.key, page: e.value))
+        .toList();
   }
 
   @override
