31a9843cce0046df03ccab09c958519ad13000a3
Jonathan Ehwald <github@ehwald.info>
2022-01-25 04:50:05 +0100

    refactor: move app into separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_callkeep/flutter_callkeep.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:stream_testing/app.dart';
 import 'package:stream_testing/models/gesture.dart';
-import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,17 +10,5 @@ void main() async {
   Hive.registerAdapter<Gesture>(GestureAdapter());
   await Hive.openBox<Gesture>("gestures");
   await CallKeep.setup();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
-  }
+  runApp(const App());
 }
